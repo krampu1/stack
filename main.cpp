52a3115a8cab30e4b76bb0f4a7dd5e4bd45d4dac
krampu1 <24354657a@gmail.com>
2022-09-26 05:09:29 +0300

    first version

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <stdio.h>
+
+#define Type_t int
 #include "stack/stack.h"
 
 int main() {
     Stack stack = {};
-    if (stack_push(&stack, 1) == 0) {
-        printf("error");
+    add_info_where_created(stack);
+    
+    for (int i = 0; i < 20; i++) {
+        stack_push(&stack, i);
     }
+    
+    printf("%d\n", stack_pop(&stack));
+
+    for (int i = 0; i < 20 ; i++) {
+        stack_push(&stack, i);
+    }
+
+    stack_dump((&stack));
+
+    for (int i = 0; stack.size; i++) {
+        printf("%d ", stack_pop(&stack));
+    }
+
+    stack_del(&stack);
+    
+    stack_pop(&stack);
 }
--- stack/stack.h
+++ stack/stack.h
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 #undef DEFOLT_STACK_SIZE
-#define DEFOLT_STACK_SIZE 10
 
 #undef struct
 #undef size_t
@@ -11,16 +10,42 @@
 #undef size
 #undef capacity
 
-#ifndef Type_t
-#define Type_t int
-#endif
+enum Stack_defoult_values{
+    DEFOLT_STACK_SIZE = 10,
+    STACK_DEL_PTR     = 1337,
+};
 
-///
-#define stack_error(stack) true
-#define stack_dump(stack) printf("assert\n")
-#define memory_alloc_error()
-///
+#define add_info_where_created(stack) stack.name = (char *)#stack;\
+                                      stack.line = (size_t)__LINE__;\
+                                      stack.file = (char *)__FILE__;\
+                                      stack.func = (char *)__FUNCTION__;
 
+#define stack_error(stack) stack->size > stack->capacity || stack->data == (Type_t *)STACK_DEL_PTR
+
+#define stack_dump(stack) {FILE *ptr_log_file = fopen("log.out", "a");\
+                           fprintf(ptr_log_file, "in %s() at %s(%ld):\n", __FUNCTION__, __FILE__, __LINE__);        \
+                           fprintf(ptr_log_file, "  Stack(%p) \"%s\"\n", stack, #stack);                            \
+                           fprintf(ptr_log_file, "  \"%s\" at %s() at %s\n", stack->name, stack->func, stack->file);\
+                           fprintf(ptr_log_file, "  {\n    size = %ld\n", stack->size);                             \
+                           fprintf(ptr_log_file, "    capacity = %ld\n",  stack->capacity);                         \
+                           fprintf(ptr_log_file, "    data[%p]\n",        stack->data);                             \
+                                                                                                                    \
+                           for (size_t i = 0; i < stack->capacity; i++) {                                           \
+                               if (i < stack->size) {                                                               \
+                                   fprintf(ptr_log_file, "    *[%ld]=%d\n", i, stack->data[i]);                     \
+                               }                                                                                    \
+                               else {                                                                               \
+                                   fprintf(ptr_log_file, "     [%ld]=%d\n", i, stack->data[i]);                     \
+                               }                                                                                    \
+                           }                                                                                        \
+                           fprintf(ptr_log_file, "  }\n");                                                          \
+                           fclose(ptr_log_file);                                                                    \
+                          }
+
+#define memory_alloc_error() {FILE *ptr_log_file = fopen("log.out", "a");                                                             \
+                              fprintf(ptr_log_file, "memory allocation error in %s() at %s(%ld)\n", __FUNCTION__, __FILE__, __LINE__);\
+                              fclose(ptr_log_file);                                                                                   \
+                             }
 
 #ifndef NDEBUG
 #define stack_assert(stack) if (stack_error(stack)) stack_dump(stack)
@@ -32,14 +57,18 @@ struct Stack{
     Type_t *data    = nullptr;
     size_t size     = 0;
     size_t capacity = 0;
+    char *name      = nullptr;
+    size_t line     = 0;
+    char *file      = nullptr;
+    char *func      = nullptr;
 };
 
 size_t stack_resize(Stack *stack) {
     stack_assert(stack);
-
+    
     if (stack->capacity < DEFOLT_STACK_SIZE) {
-        stack->data = (Type_t *)realloc(stack->data, 10 * sizeof(Type_t));
-
+        stack->data = (Type_t *)realloc(stack->data, DEFOLT_STACK_SIZE * sizeof(Type_t));
+        
         if (stack->data == nullptr) {
              memory_alloc_error();
              return 0;
@@ -81,10 +110,40 @@ size_t stack_push(Stack* stack, Type_t a) {
     if (stack_resize(stack) == 0) {
         return 0;
     }
-
+    
     *(stack->data+(stack->size++)) = a;
 
     stack_assert(stack);
 
     return stack->size;
+}
+
+Type_t stack_pop(Stack* stack) {
+    stack_assert(stack);
+
+    if (stack->size == 0) {
+        stack_dump(stack);
+
+        return 0;
+    }
+
+    Type_t elem = stack->data[stack->size-- - 1];
+
+    stack_resize(stack);
+
+    stack_assert(stack);
+
+    return elem;
+}
+
+void stack_del(Stack* stack) {
+    stack_assert(stack);
+
+    realloc(stack->data, 0);
+
+    stack->data = (Type_t *)STACK_DEL_PTR;
+
+    stack->size = 1;
+
+    stack->capacity = 0;
 }
